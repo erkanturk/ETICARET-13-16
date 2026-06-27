1fb0c9a4732c87e89bb355d629ae8c832076e6b2
erkanturk <refres98@gmail.com>
2026-06-27 13:09:58 +0300

    Seed yapısı

--- ETICARET/ETICARET.DataAccess/Concrete/EfCore/SeedDatabase.cs
+++ ETICARET/ETICARET.DataAccess/Concrete/EfCore/SeedDatabase.cs
@@ -1,10 +1,23 @@
-﻿using System;
+﻿using ETICARET.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ETICARET.DataAccess.Concrete.EfCore
 {
-    internal class SeedDatabase
+    public static class SeedDatabase
     {
+        public static void Seed()
+        {
+            var context = new DataContext();
+
+        }
+        private static Category[] Categories =
+        {
+            new Category(){Name="Telefon"},
+            new Category(){Name="Bilgisayar"},
+            new Category(){Name="Elektronik"},
+            new Category(){Name="Ev Gereçleri"}
+        };
     }
 }
