a9eb4e607c954ec726ccf8016a9a67786e694513
erkanturk <refres98@gmail.com>
2026-06-28 15:15:32 +0300

    Extensions

--- ETICARET/ETICARET.WebUI/EmailService/MailHelper.cs
+++ ETICARET/ETICARET.WebUI/EmailService/MailHelper.cs
@@ -1,6 +1,43 @@
-﻿namespace ETICARET.WebUI.EmailService
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace ETICARET.WebUI.EmailService
 {
-    public class MailHelper
+    public static class MailHelper
     {
+        public static bool SendMail(string body, string to, string subject, bool isHtml = true)
+        {
+            return SendMail(body, [to], subject, isHtml);
+        }
+        private static bool SendMail(string body, List<string> to, string subject, bool isHtml)
+        {
+            bool result = false;
+            try
+            {
+                using var message = new MailMessage();
+                message.From = new MailAddress("ubymailsistem@gmail.com");
+                to.ForEach(x => message.To.Add(new MailAddress(x)));
+                message.Subject = subject;
+                message.Body= body;
+                message.IsBodyHtml = isHtml;
+
+                using var smtp = new SmtpClient("smtp.gmail.com", 587);
+                smtp.EnableSsl = true;
+                smtp.Credentials = new NetworkCredential(
+                        "ubymailsistem@gmail.com",
+                        "devr heai vcqx zdzr"//uygulama şifresi.
+                    );
+                smtp.UseDefaultCredentials = false;
+                smtp.Send(message);
+                result = true;
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"Mail gönderim hatası: {ex.Message}");
+            }
+            return result;
+        }
     }
 }
